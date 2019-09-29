39ba66824c518bf6af400acd593bf7d964b4def8
aleksitolvanen <aleksi.tolvanen@iki.fi>
2019-09-29 16:10:20 +0300

    Initial version (WIP)

---
+++ WpfNotifyIconScriptsRunner/Commands/SetDefaultSoundDeviceCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfNotifyIconScriptsRunner.Commands
+{
+    /// <summary>
+    /// Command that calls nircmd.dll setdefaultsounddevice with given parameter
+    /// to set default sound device
+    /// </summary>
+    public class SetDefaultSoundDeviceCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            NirCmdCall.DoNirCmd($"setdefaultsounddevice \"{parameter}\"");
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+    }
+}
---
+++ WpfNotifyIconScriptsRunner/NirCmdCall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WpfNotifyIconScriptsRunner
+{
+    public class NirCmdCall
+    {
+        // x64
+        [DllImport("nircmd.dll")]
+        public static extern bool DoNirCmd(String NirCmdStr);
+    }
+}
